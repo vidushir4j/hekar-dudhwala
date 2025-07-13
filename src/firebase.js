d0f10aeadfb69ed4f524aca9ee86c858be50edab
vidushir4j <rajsharmavidushi@gmail.com>
2025-07-13 13:59:39 +0530

    Fixed Firestore analytics usage

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 
-export { analytics };
